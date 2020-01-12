d641797719fbcd58ea5a276382e06cfaf0f7abe1
陈昶安 <chenchangan@chenchangandeMacBook-Pro.local>
2020-01-12 12:28:10 -0800

    router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Switch, Route } from 'react-router-dom';
+
 import './App.css';
 import HomePage from './pages/homepage/homepage.component'
 function App() {
   return (
-    <div className="App">
-      <HomePage/>
+    <div>
+      <Switch>
+        <Route exact={true} path='/' component={HomePage}/>
+      </Switch>
     </div>
   );
 }
