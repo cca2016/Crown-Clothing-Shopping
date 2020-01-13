c5d79100dea72c0b0f24ec0abaad94d74c01e1c3
陈昶安 <chenchangan@chenchangandeMacBook-Pro.local>
2020-01-13 00:06:39 -0800

    sign in and sign up

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ class App extends React.Component{
   unsubscribeFromAuth = null;
 
   componentDidMount(){
-    this.unsubscribeFromAuthauth = auth.onAuthStateChanged(async userAuth => {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if (userAuth){
         const userRef = await createUserProfileDocument(userAuth);
 
@@ -32,7 +32,7 @@ class App extends React.Component{
               id: snapShot.id,
               ...snapShot.data()
             }
-          })
+          }, ()=>{console.log(this.state);})
         });
       } else {
         this.setState({ currentUser: userAuth});
